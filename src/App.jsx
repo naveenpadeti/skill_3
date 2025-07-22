b0c3e4ff40e966c7d5e8017c755a103fed3cb9fd
naveenpadeti <naveenpadeti123@gmail.com>
2025-07-22 15:29:37 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/s207">
+    <Router basename="/skill_3">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
